48ccc9adb04da35d0f54ba24eb0e5ccf5f8d1bc7
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-19 09:54:16 +0700

    Create Model for User

---
+++ project/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+//create schema
+const UserSchema = new schema({
+  name: {
+    type: string,
+    required: true
+  },
+  email: {
+    type: string,
+    required: true
+  },
+  password: {
+    type: string,
+    required: true
+  },
+  avatar: {
+    type: string,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
