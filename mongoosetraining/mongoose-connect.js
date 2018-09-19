4839e5c5717b9d5ad506229858851c72ab58855e
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-20 05:46:32 +0700

    Mongoose Connection

---
+++ mongoosetraining/mongoose-connect.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const { connection, Schema } = mongoose;
+
+mongoose.connect("mongodb://localhost:27017");
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("connected!");
+});
