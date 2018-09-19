3e52fdd15341a858ce3ec16ff9e04599eedf9e51
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-19 23:40:40 +0700

    addUser on Restful

--- Restful-basic/test-addUser.js
+++ Restful-basic/test-addUser.js
@@ -5,7 +5,7 @@ const r = async (url, method) =>
 const log = (...obj) => obj.forEach(o => console.dir(o, { colors: true }));
 
 async function addUser() {
-  const addUsr = await r(`/users/Smith`, "post");
+  const addUsr = await r(`/users/Gun`, "post");
   log(`[POST] a new user:`, addUsr);
   const getAll = await r("/users/all", "get");
   log(`[GET] users:`, getAll);
