3cbea450445e32a162beadce68a6ce5338e6e55d
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-19 10:02:12 +0700

    Add Body Parser Middleware

--- project/routes/api/users.js
+++ project/routes/api/users.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+// Load User Model
+const user = require("../../models/User");
+
+// @route   GET api/users/test
+// @desc    Test Users Route
+// @access  Public
 router.get("/test", (req, res) => res.json({ msg: "User Works" }));
+
+// @route   GET api/users/register
+// @desc    Register Users
+// @access  Public
+router.post("/register", (req, res) => {
+  //check email first
+  User.findOne({email: req.body.email})
+});
 
 module.exports = router;
--- project/server.js
+++ project/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const users = require('./routes/api/Users');
-const profile = require('./routes/api/Profile');
-const posts = require('./routes/api/Posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -19,9 +24,9 @@ mongoose
 app.get("/", (req, res) => res.send("Hello World"));
 
 // Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
