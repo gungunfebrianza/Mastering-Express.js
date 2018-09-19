6bc920432f0f0fdefb412d9ad329e2da9a84f098
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-19 10:56:44 +0700

    Add Step 2 History

--- project/History/Step2/server.js
+++ project/History/Step2/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
+
+// Body Parser Middleware
+// If extended is false, you can not post "nested object"
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -18,10 +24,10 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World"));
 
-// // Use Routes
-// app.use('/api/users', users);
-// app.use('/api/profile', profile);
-// app.use('/api/posts', posts);
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
