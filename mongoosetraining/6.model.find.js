d6b2897de890258fe0bb242a04b7666ff07daaee
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-21 00:39:22 +0700

    Querying Technique find()

--- mongoosetraining/6.model.find.js
+++ mongoosetraining/6.model.find.js
@@ -3,11 +3,11 @@ const { Schema } = mongoose;
 
 //mongoose.connect("mongodb://localhost:27017/training");
 //support multi-connection
-var connection1 = mongoose.createConnection(
+var mongooseConnection = mongoose.createConnection(
   "mongodb://localhost:27017/training"
 );
 
-const db = connection1;
+const db = mongooseConnection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
   console.log("connected!");
@@ -25,13 +25,11 @@ var schema = new Schema({
 });
 
 // 2. Compile Schema
-var ModelMan = connection1.model("Man", schema);
+var ModelMan = mongooseConnection.model("Man", schema);
 
 // 3. Querying
 //we want to display all specific the tank we've seen.
-ModelMan.find({ name: "Qrista" }, function(err, man) {
+ModelMan.find(function(err, man) {
   if (err) return console.error(err);
-  console.log(typeof man);
-  console.log(man[0]);
-  console.log(man[0].name);
+  console.log(man);
 });
--- mongoosetraining/7.model.find-certain.js
+++ mongoosetraining/7.model.find-certain.js
@@ -27,18 +27,55 @@ var schema = new Schema({
 // 2. Compile Schema
 var ModelMan = mongooseConnection.model("Man", schema);
 
-// 3. Querying
-// Retrieving only certain fields
-ModelMan.find({ name: "Qrista" }, "age", function(err, tanks) {
-  if (err) return console.error(err);
-  console.log(tanks);
-});
+/*Mantra
+Model.find()
+Parameters
+    conditions «Object»
+    [projection] «Object|String» optional fields to return, see Query.prototype.select()
+    [options] «Object» optional see Query.prototype.setOptions()
+    [callback] «Function»
+Returns:
+    «Query»
+See More at : https://mongoosejs.com/docs/api.html#model_Model.find
+*/
 
-// 3. Querying
-//we want to display all specific the tank we've seen.
+// 3. Querying Condition
 ModelMan.find({ name: "Qrista" }, function(err, man) {
   if (err) return console.error(err);
   console.log(typeof man);
   console.log(man[0]);
   console.log(man[0].name);
+});
+
+// 4. Querying Condition With Comparison Operator
+//$gte selects the documents where the value of the field is greater than or equal to (i.e. >=) a specified value (e.g. value.)
+ModelMan.find({ name: "YumaTOLOL", age: { $gte: 30 } }, function(err, man) {
+  if (err) return console.error(err);
+  console.log(man);
+});
+
+// 5. Querying Condition With Logical Operator
+//Using the $or operator, you can specify a compound query that joins each clause with a logical OR conjunction so that the query selects the documents in the collection that match at least one condition.
+ModelMan.find(
+  {
+    $or: [{ name: "YumaTOLOL" }, { name: "YumaTOL" }]
+  },
+  function(err, man) {
+    if (err) return console.error(err);
+    console.log(man);
+  }
+);
+
+// 6. Querying with Projection
+// Retrieving only certain fields
+ModelMan.find({ name: "Qrista" }, "age", function(err, man) {
+  if (err) return console.error(err);
+  console.log(man);
+});
+
+// 7. Querying with Option
+// Retrieving only certain fields
+ModelMan.find({ name: "YumaTOLOL" }, null, { skip: 1 }, function(err, man) {
+  if (err) return console.error(err);
+  console.log(man);
 });
