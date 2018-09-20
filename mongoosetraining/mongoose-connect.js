d5c4a7c8f71b50ab61a231239d825ff50a42b281
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-20 11:17:44 +0700

    Add Mongoose Official Example

--- mongoosetraining/mongoose-connect.js
+++ mongoosetraining/mongoose-connect.js
@@ -3,7 +3,7 @@ const { connection, Schema } = mongoose;
 
 mongoose.connect("mongodb://localhost:27017");
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
   console.log("connected!");
---
+++ mongoosetraining/mongoose-official.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { connection, Schema } = mongoose;
+
+mongoose.connect("mongodb://localhost:27017/nodeauth");
+
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+kittySchema.methods.speak = function() {
+  var greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name";
+  console.log(greeting);
+};
+
+var Kitten = mongoose.model("Kitten", kittySchema);
+
+var miaw = new Kitten({ name: "qrista qhuluk" });
+console.log(miaw.name); // 'qrista qhuluk'
+
+var fluffy = new Kitten({ name: "fluffy" });
+fluffy.speak(); // "Meow name is fluffy"
+
+//Each document can be saved to the database by calling its save method.
+fluffy.save(function(err, fluffy) {
+  if (err) return console.error(err);
+  console.log("Data Saved!");
+});
+
+//we want to display all the kittens we've seen.
+Kitten.find(function(err, kittens) {
+  if (err) return console.error(err);
+  console.log(kittens);
+});
