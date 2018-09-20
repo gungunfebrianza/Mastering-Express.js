c13ba187b7442c3b4b9b69d6d81a0e19baad7173
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-20 21:37:35 +0700

    Rearrangement Script

--- mongoosetraining/test.js
+++ mongoosetraining/test.js
@@ -16,7 +16,13 @@ db.once("open", function() {
 // 1. Create Schema
 var schema = new Schema({
   name: "string",
-  size: "string"
+  size: { type: String },
+  age: {
+    type: Number,
+    get: v => Math.round(v),
+    set: v => Math.round(v),
+    alias: "i"
+  }
 });
 
 // 2. Compile Schema
@@ -25,7 +31,8 @@ var ModelTank = connection.model("Tank", schema);
 // 3. Create Document
 var DocumentBig = new ModelTank({
   name: "big",
-  size: "big"
+  size: "big",
+  age: 20.002
 });
 
 DocumentBig.save(function(err) {
@@ -35,7 +42,8 @@ DocumentBig.save(function(err) {
 
 // 4. Querying
 //we want to display all the tank we've seen.
-ModelTank.find(function(err, tanks) {
+ModelTank.find({ age: 20 }, function(err, tanks) {
   if (err) return console.error(err);
   console.log(tanks);
 });
+
