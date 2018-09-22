1a16a76bb9275e516e32dc71feb18672aba7fccd
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-23 03:25:34 +0700

    create validation function

---
+++ project/validation/is-empty.js
@@ -0,0 +1,8 @@
+function isEmpty(value) {
+  return (
+    value === undefined ||
+    value === null ||
+    (typeof value === "object" && Object.keys(value).length === 0) ||
+    (typeof value === "string" && value.trim().length === 0)
+  );
+}
