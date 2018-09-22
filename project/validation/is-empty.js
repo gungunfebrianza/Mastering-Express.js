7d95768dd079805f4cad87fa1bee796d11896b0a
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-23 04:02:03 +0700

    Update is-empty.js

--- project/validation/is-empty.js
+++ project/validation/is-empty.js
@@ -1,8 +1,25 @@
-function isEmpty(value) {
-  return (
-    value === undefined ||
-    value === null ||
-    (typeof value === "object" && Object.keys(value).length === 0) ||
-    (typeof value === "string" && value.trim().length === 0)
-  );
-}
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+module.exports = isEmpty;
+
+// const isEmpty = value => {
+//   return (
+//     value === undefined ||
+//     value === null ||
+//     (typeof value === "object" && Object.keys(value).length === 0) ||
+//     (typeof value === "string" && value.trim().length === 0)
+//   ); //trim Remove whitespace from both sides of a string:
+// };
+
+// function isEmpty(value) {
+//   return (
+//     value === undefined ||
+//     value === null ||
+//     (typeof value === "object" && Object.keys(value).length === 0) ||
+//     (typeof value === "string" && value.trim().length === 0)
+//   ); //trim Remove whitespace from both sides of a string:
+// }
