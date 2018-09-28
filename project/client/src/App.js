f94ea57107cddeefcdef218f1b07421cbf4fb465
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-28 08:42:09 +0700

    Add React Application

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/Login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>;
   }
 }
 
---
+++ project/client/src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <footer className="bg-dark text-white mt-5 p-4 text-center">
+        copyright &copy; {new Date().getFullYear()} DevDorm
+      </footer>
+    </div>
+  );
+};
+
