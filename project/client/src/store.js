4ccf2678a768ab2b767e96884e7ac4b60419c5de
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-28 21:33:53 +0700

    Add Reducers

--- project/client/src/store.js
+++ project/client/src/store.js
@@ -1,8 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
+const initialState = {};
 const middleware = [thunk];
 
-const store = createStore(() => [], {}, applyMiddleware(...middleware));
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
