c5f34359f50de92f2a481074fb135cbfd0ef7c07
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-11 23:11:05 +0700

    improve

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
+// Component
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import './App.css';
+
+const middleware = [thunk];
+const reduxStore = createStore(() => [], {}, applyMiddleware(...middleware));
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Provider store={reduxStore}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
