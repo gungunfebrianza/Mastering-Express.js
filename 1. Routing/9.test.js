5c505c37fcb4238a0f7c4b31dd091d9c3ea5d300
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-18 17:17:25 +0700

    Modular Router

--- 1. Routing/9.test.js
+++ 1. Routing/9.test.js
@@ -1,11 +1,10 @@
-const express = require('express')
-var birds = require('./8.module-bird')
+const express = require('express');
+const app = express();
+var birds = require('./8.module-bird');
 
-const app = express()
+app.use('/birds', birds);
+app.get('/', (req, res, next) => {
+  res.send('Home');
+});
 
-app.use('/birds', birds)
-
-app.listen(
-	9999,
-	() => console.log('Web Server running on port 9999'),
-)
+app.listen(9999, () => console.log('Web Server running on port 9999'));
