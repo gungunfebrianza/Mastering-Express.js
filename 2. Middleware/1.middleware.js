c0f52fb1213fd0179664743f11362c02b6406ba7
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-20 17:09:04 +0700

    Display Output Logger

--- 2. Middleware/1.middleware.js
+++ 2. Middleware/1.middleware.js
@@ -1,27 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const fs = require('fs');
 
-var myLogger = function (req, res, next) {
+var myLogger = function(req, res, next) {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
-  fs.appendFile('server.log', log + '\n', (err) => {
+  fs.appendFile('server.log', log + '\n', err => {
     if (err) {
-      console.log('Unable to append to server.log.')
+      console.log('Unable to append to server.log.');
     }
   });
-  next()
-}
+  next();
+};
 
 /*To load the middleware function, call app.use(), specifying the middleware
 function. For example, the following code loads the myLogger middleware
 function before the route to the root path (/).*/
-app.use(myLogger)
+app.use(myLogger);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
 
 app.listen(9999, function() {
   console.log('Listening on port 9999!');
 });
+
+// Output :
+// Sat Oct 20 2018 17:08:05 GMT+0700 (Western Indonesia Time): GET /
+// Sat Oct 20 2018 17:08:06 GMT+0700 (Western Indonesia Time): GET /favicon.ico
