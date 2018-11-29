ae0864cd32bc9078a3aaa69946409881bd566a49
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-29 20:16:53 +0700

    Transfer History

--- project/client/src/components/auth/Register.js
+++ project/client/src/components/auth/Register.js
@@ -40,7 +40,7 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    this.props.registerUser(newUser);
+    this.props.registerUser(newUser, this.props.history);
   }
 
   render() {
