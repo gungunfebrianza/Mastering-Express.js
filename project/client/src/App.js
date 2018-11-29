f3de77e7aabdee9b32495a85ad3b41c3af89ccc9
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-29 08:16:18 +0700

    Setup Redex Dev Tools

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -19,7 +19,10 @@ const middleware = [thunk];
 const reduxStore = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(...middleware)
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 class App extends Component {
