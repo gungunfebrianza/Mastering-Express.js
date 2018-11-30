88580b67b44e84c523cc50f4ccfd21a82bb08420
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-01 04:49:27 +0700

    Add Component to Main App

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -21,6 +21,7 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 
 import './App.css';
 
@@ -82,6 +83,13 @@ class App extends Component {
                   exact
                   path="/edit-profile"
                   component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
                 />
               </Switch>
             </div>
