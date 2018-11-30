1d623de9134d30f268e3e7bc276abe142552d345
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-30 07:24:04 +0700

    Add Dashboard Component

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -15,6 +15,7 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
 
@@ -59,6 +60,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
