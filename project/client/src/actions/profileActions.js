efeaf23451215fb11680ed50df1cbb6fc5e2d7a9
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-01 06:22:02 +0700

    Profile Action

--- project/client/src/actions/profileActions.js
+++ project/client/src/actions/profileActions.js
@@ -40,6 +40,18 @@ export const createProfile = (profileData, history) => dispatch => {
     );
 };
 
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post('/api/profile/experience', expData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Delete Account & Profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm('are you sure this cannot be undone?')) {
