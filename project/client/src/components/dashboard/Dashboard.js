430ef63c76b0dbe2bb49d54d3d4d379e6d8cb233
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-30 19:12:03 +0700

    Add ProfileActions

--- project/client/src/components/dashboard/Dashboard.js
+++ project/client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
+import ProfileActions from './ProfileActions'
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -21,7 +22,13 @@ class Dashboard extends Component {
     } else {
       // Check if logged in user has profile data
       if (Object.keys(profile).length > 0) {
-        dashboardContent = <h4>TODO: DISPLAY PROFILE</h4>;
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">
+              Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>{' '}
+            </p>
+          </div>
+        );
       } else {
         // User is logged in but has no profile
         dashboardContent = (
@@ -62,4 +69,7 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile }
+)(Dashboard);
