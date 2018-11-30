298c53740a444d12de541a302f48c786792f54d7
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-11-30 07:04:27 +0700

    Add Proptypes and Lifecycle

--- project/client/src/components/layout/Landing.js
+++ project/client/src/components/layout/Landing.js
@@ -4,6 +4,11 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Landing extends Component {
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
   render() {
     return (
       <div className="landing">
@@ -32,6 +37,10 @@ class Landing extends Component {
     );
   }
 }
+
+Landing.propTypes = {
+  auth: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
   auth: state.auth
