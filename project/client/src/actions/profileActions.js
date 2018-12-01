07b7c7d394fd9951901440eaa4a0fc07f8b0626a
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-01 07:20:45 +0700

    Display All Profiles

--- project/client/src/actions/profileActions.js
+++ project/client/src/actions/profileActions.js
@@ -5,7 +5,8 @@ import {
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
-  SET_CURRENT_USER
+  SET_CURRENT_USER,
+  GET_PROFILES
 } from './types';
 
 // Get current profile
@@ -98,6 +99,21 @@ export const deleteAccount = () => dispatch => {
       )
     );
   }
+};
+
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios.get('/api/profile/all').then(res =>
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    }).catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
+      })
+    )
+  );
 };
 
 // Profile loading
