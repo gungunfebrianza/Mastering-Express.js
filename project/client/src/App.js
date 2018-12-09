7134c477573908eed81c21a47d67dd76345fc61c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-09 16:13:06 +0700

    Add Post Reducer

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -24,6 +24,7 @@ import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 
 import './App.css';
 
@@ -71,6 +72,7 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
---
+++ project/client/src/reducers/postReducer.js
@@ -0,0 +1,5 @@
+const initialState = {
+  posts: [],
+  post: {},
+  loading: false
+};
