d9a9ebcebec6ac3b6e48d153380bc2bd55dab85a
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-10 01:31:12 +0700

    Add Component to App

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -25,6 +25,8 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Post';
+import NotFound from './components/not-found/NotFound';
 
 import './App.css';
 
@@ -104,6 +106,10 @@ class App extends Component {
                   component={AddEducation}
                 />
               </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
+              </Switch>
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
