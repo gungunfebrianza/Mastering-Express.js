3d43ae1eb3075f67e31bbcd316a39307010f9610
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-10 11:09:18 +0700

    Lifecycle Component

--- project/client/src/components/posts/PostForm.js
+++ project/client/src/components/posts/PostForm.js
@@ -16,13 +16,28 @@ class PostForm extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(newProps) {
+    if (newProps.errors) {
+      this.setState({ errors: newProps.errors });
+    }
+  }
+
   onSubmit(e) {
     e.preventDefault();
+    const { user } = this.props.auth;
 
+    const newPost = {
+      text: this.state.text,
+      name: user.name,
+      avatar: user.avatar
+    };
+
+    this.props.addPost(newPost);
+    this.setState({ text: '' });
   }
 
   onChange(e) {
-    this.setState({[e.target.name]: e.target.value})
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
@@ -58,11 +73,14 @@ PostForm.propTypes = {
   addPost: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   auth: state.auth,
   errors: state.errors
-})
+});
 
-export default connect(mapStateToProps, {addPost})(PostForm);
+export default connect(
+  mapStateToProps,
+  { addPost }
+)(PostForm);
