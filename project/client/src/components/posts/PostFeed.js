8130f0c8b322b18bd9a46ec09a335ec1e25e1729
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-11 00:24:06 +0700

    Post Feed Component

---
+++ project/client/src/components/posts/PostFeed.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import PostItem from './PostItem';
+
+class PostFeed extends Component {
+  render() {
+    const { posts } = this.props;
+    return posts.map(post => <PostItem key={post.id} post={post} />);
+  }
+}
+
+PostFeed.propTypes = {
+  posts: PropTypes.array.isRequired
+};
+
+export default PostFeed;
