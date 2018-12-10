9b05d59fbf303ed04943f7d0b683bfcd8f80060e
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-10 23:56:20 +0700

    Implement Loading on Get Data

--- project/client/src/actions/postAction.js
+++ project/client/src/actions/postAction.js
@@ -5,21 +5,12 @@ import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from './types';
 export const addPost = postData => dispatch => {
   axios
     .post('/api/posts', postData)
-    .then(res =>
-      dispatch({
-        type: ADD_POST,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+    .then(res => dispatch({ type: GET_POST, payload: res.data }))
+    .catch(err => dispatch({ type: GET_POST, payload: null }));
 };
 
 export const getPost = () => dispatch => {
+  dispatch(setPostLoading());
   axios
     .post('/api/posts', postData)
     .then(res =>
