4a8975d0b3215888c63ad6e7b43fbebd9d93e395
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-11 00:02:32 +0700

    Fix Bugs

--- project/client/src/actions/postAction.js
+++ project/client/src/actions/postAction.js
@@ -5,14 +5,6 @@ import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from './types';
 export const addPost = postData => dispatch => {
   axios
     .post('/api/posts', postData)
-    .then(res => dispatch({ type: GET_POST, payload: res.data }))
-    .catch(err => dispatch({ type: GET_POST, payload: null }));
-};
-
-export const getPost = () => dispatch => {
-  dispatch(setPostLoading());
-  axios
-    .post('/api/posts', postData)
     .then(res =>
       dispatch({
         type: ADD_POST,
@@ -25,6 +17,14 @@ export const getPost = () => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+export const getPost = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .post('/api/posts')
+    .then(res => dispatch({ type: GET_POSTS, payload: res.data }))
+    .catch(err => dispatch({ type: GET_POSTS, payload: null }));
 };
 
 //Set Loading State
