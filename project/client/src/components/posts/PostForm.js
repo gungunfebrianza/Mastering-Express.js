203aeee4ad49be7b12c1ba5e9b5164b389457c36
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-10 10:33:21 +0700

    Errors Variable Before Render

--- project/client/src/components/posts/PostForm.js
+++ project/client/src/components/posts/PostForm.js
@@ -1,24 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import TextAreaFieldGroup from '../common/TextAreaFieldGroup'
-import {addPost} from '../../actions/postAction'
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+import { addPost } from '../../actions/postAction';
 
 class PostForm extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       text: '',
       errors: {}
-    }
+    };
   }
 
   render() {
+    const { errors } = this.state;
+
     return (
-      <div>
-        
+      <div className="post-form mb-3">
+        <div className="card card-info">
+          <div className="card-header bg-info text-white">Say Somthing...</div>
+          <div className="card-body">
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <TextAreaFieldGroup
+                  placeholder="Create a post"
+                  name="text"
+                  value={this.state.text}
+                  onChange={this.onChange}
+                  error={errors.text}
+                />
+              </div>
+              <button type="submit" className="btn btn-dark">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
