d4e90addfb8b478611e9e51671aedeea10b6dae3
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-10 11:05:20 +0700

    propTypes & mapStateProps

--- project/client/src/components/posts/PostForm.js
+++ project/client/src/components/posts/PostForm.js
@@ -54,4 +54,15 @@ class PostForm extends Component {
   }
 }
 
-export default PostForm;
+PostForm.propTypes = {
+  addPost: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+})
+
+export default connect(mapStateToProps, {addPost})(PostForm);
