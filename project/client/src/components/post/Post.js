1dc8d9b49df2ba89b5512031a0349d1a33fd53be
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-12-11 01:41:20 +0700

    Update Post

--- project/client/src/components/post/Post.js
+++ project/client/src/components/post/Post.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import PostItem from '../posts/PostItem';
 import Spinner from '../common/Spinner';
 import { getPosts } from '../../actions/postAction';
 
 class Post extends Component {
+  componentDidMount() {
+    this.props.getPosts(this.props.match.param.id);
+  }
+
   render() {
+    const { post, loading } = this.props;
+    let postContent;
+
+    if (post === null || loading || Object.keys(post).length === 0) {
+      postContent = <Spinner />;
+    } else {
+      postContent = (
+        <div>
+          <PostItem post={post} />
+        </div>
+      );
+    }
     return (
-      <div>
-        <h1>POST</h1>
+      <div className="post">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <Link to="/feed" className="btn btn-light mb-3">
+                Back
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
