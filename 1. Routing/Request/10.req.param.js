e9cefbc34197f4d09cfb7687ee4804702959cd63
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-05-08 00:37:34 +0700

    param

--- 1. Routing/Request/10.req.param.js
+++ 1. Routing/Request/10.req.param.js
@@ -1,7 +1,7 @@
 const express = require('express');
 let app = express();
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   res.send('home');
 });
 
@@ -10,12 +10,20 @@ This property is an object containing properties mapped to the named route “pa
 For example, if you have the route /user/:name, then the “name” property is available as req.params.name.
 This object defaults to {}.
 */
-app.get('/post/:id/category/:category_id', (req,res)=>{
+app.get('/post/:id/category/:category_id', (req, res) => {
   res.send(`
     <p>Here is ${req.params.id}</p>
     <p>category is ${req.params.category_id}</p>
     `);
 });
+
+/* Visit: 
+localhost:9999/post/2/category/3; 
+*/
+
+/* Output :
+<p>Here is 2</p>
+<p>category is 3</p> */
 
 app.listen(9999);
 console.log('server listening!');
