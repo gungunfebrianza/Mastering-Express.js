d6c5c51ac6d57f432d8d16e996d71435779b32bf
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-05-08 00:02:58 +0700

    Examine Header

--- 1. Routing/Request/1.req.header.js
+++ 1. Routing/Request/1.req.header.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log(JSON.stringify(req.headers, null, 2));
+  console.log(JSON.stringify(req.headers, null, 3));
   res.json({
     headers: req.headers,
     userAgent: req.get('user-agent')
@@ -11,7 +11,35 @@ app.get('/', (req, res) => {
 
 app.listen(9999, () => console.log('Web Server running on port 9999'));
 
-/* Notes :
+//OUTPUT :
+// {
+//     "headers": {
+//         "user-agent": "PostmanRuntime/7.11.0",
+//         "accept": "*/*",
+//         "cache-control": "no-cache",
+//         "postman-token": "08c0b3b1-0663-49df-a45d-b32749acfbe3",
+//         "host": "localhost:9999",
+//         "accept-encoding": "gzip, deflate",
+//         "connection": "keep-alive"
+//     },
+//     "userAgent": "PostmanRuntime/7.11.0"
+// }
+
+// {
+//   "headers": {
+//     "host": "localhost:9999",
+//     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+//     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+//     "accept-language": "en-US,en;q=0.5",
+//     "accept-encoding": "gzip, deflate",
+//     "connection": "keep-alive",
+//     "cookie": "_ga=GA1.1.1797838580.1557074751",
+//     "upgrade-insecure-requests": "1"
+//   },
+//   "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"
+// }
+
+/* Notes JSON Stringify :
 1. Client request contain all kinds of headers that tell the server useful information about the request. 
 
 2. Request headers depend on the method of the request such as GET, and POST. 
